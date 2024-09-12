fe302884291d0b87577f0932b48dab753d905103
soyeon1221 <ma0302@naver.com>
2024-09-12 23:53:41 +0900

    Updates

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,3 @@
-import tiger from './img/doll_tiger.jpg';
-import haagendazs from './img/props_haagendazs.jpg';
 
 export default function Main() {
   return (
@@ -8,30 +6,72 @@ export default function Main() {
         <div className="main_wrap">
           <ul className="main_box">
             <li className="main_items">
-              <img src={tiger} />
+              <img src={process.env.PUBLIC_URL + '/images/호랑이 인형.jpg'} />
               <div>인형</div>
             </li>
             <li className="main_items">
-              <img src={tiger} />
+              <img src={process.env.PUBLIC_URL + '/images/하얀망토 인형.jpg'} />
               <div>인형</div>
             </li>
             <li className="main_items">
-              <img src={tiger} />
+              <img src={process.env.PUBLIC_URL + '/images/티슈보관 인형.jpg'} />
               <div>인형</div>
             </li>
           </ul>
           <ul className="main_box">
             <li className="main_items">
-              <img src={haagendazs} />
-              <div>하겐다즈</div>
+              <img src={process.env.PUBLIC_URL + '/images/수원역 피규어.jpg'} />
+              <div>피규어</div>
             </li>
             <li className="main_items">
-              <img src={haagendazs} />
-              <div>하겐다즈</div>
+              <img src={process.env.PUBLIC_URL + '/images/하겐다즈.jpg'} />
+              <div>피규어</div>
             </li>
             <li className="main_items">
-              <img src={haagendazs} />
-              <div>하겐다즈</div>
+              <img src={process.env.PUBLIC_URL + '/images/훌라후프 피규어.jpg'} />
+              <div>피규어</div>
+            </li>
+          </ul>
+          <ul className="main_box">
+            <li className="main_items">
+              <img src={process.env.PUBLIC_URL + '/images/팝마트 피규어.jpg'} />
+              <div>피규어</div>
+            </li>
+            <li className="main_items">
+              <img src={process.env.PUBLIC_URL + '/images/팝마트 피규어2.jpg'} />
+              <div>피규어</div>
+            </li>
+            <li className="main_items">
+              <img src={process.env.PUBLIC_URL + '/images/팝마트 피규어3.jpg'} />
+              <div>피규어</div>
+            </li>
+          </ul>
+          <ul className="main_box">
+            <li className="main_items">
+              <img src={process.env.PUBLIC_URL + '/images/팝마트 피규어4.jpg'} />
+              <div>피규어</div>
+            </li>
+            <li className="main_items">
+              <img src={process.env.PUBLIC_URL + '/images/팝마트 피규어5.jpg'} />
+              <div>피규어</div>
+            </li>
+            <li className="main_items">
+              <img src={process.env.PUBLIC_URL + '/images/팝마트 피규어6.jpg'} />
+              <div>피규어</div>
+            </li>
+          </ul>
+          <ul className="main_box">
+            <li className="main_items">
+              <img src={process.env.PUBLIC_URL + '/images/팝마트 피규어7.jpg'} />
+              <div>피규어</div>
+            </li>
+            <li className="main_items">
+              <img src={process.env.PUBLIC_URL + '/images/동전 지갑.jpg'} />
+              <div>지갑</div>
+            </li>
+            <li className="main_items">
+              {/* <img src={process.env.PUBLIC_URL + '/images/팝마트 피규어6.jpg'} /> */}
+              <div></div>
             </li>
           </ul>
         </div>
