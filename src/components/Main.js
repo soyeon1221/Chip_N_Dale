56a4445fb4081fbb735348f7e4d32f7ca49d5b33
soyeon1221 <ma0302@naver.com>
2024-09-12 17:38:06 +0900

    Updates

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,40 @@
+import tiger from './img/doll_tiger.jpg';
+import haagendazs from './img/props_haagendazs.jpg';
+
 export default function Main() {
   return (
     <>
-      <div className="daiso">
-        <div>메인 페이지</div>
+      <div className="main">
+        <div className="main_wrap">
+          <ul className="main_box">
+            <li className="main_items">
+              <img src={tiger} />
+              <div>인형</div>
+            </li>
+            <li className="main_items">
+              <img src={tiger} />
+              <div>인형</div>
+            </li>
+            <li className="main_items">
+              <img src={tiger} />
+              <div>인형</div>
+            </li>
+          </ul>
+          <ul className="main_box">
+            <li className="main_items">
+              <img src={haagendazs} />
+              <div>하겐다즈</div>
+            </li>
+            <li className="main_items">
+              <img src={haagendazs} />
+              <div>하겐다즈</div>
+            </li>
+            <li className="main_items">
+              <img src={haagendazs} />
+              <div>하겐다즈</div>
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   )
